48a687f7924d2fe83dad2606f689c7490bb846b1
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-08-13 15:41:32 -0600

    Purchases: Create purchase view finished (js functionality finished)

--- resources/views/purchase/create.blade.php
+++ resources/views/purchase/create.blade.php
@@ -9,17 +9,16 @@
 
 @section('content')
 <div class="container-fluid px-4">
-    <h1 class="mt-4">Crear Compra</h1>
+    <h1 class="mt-4">Registrar Compra</h1>
     <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item"><a href="{{ route('panel') }}">Inicio</a></li>
         <li class="breadcrumb-item"><a href="{{ route('purchase.index') }}">Compras</a></li>
-        <li class="breadcrumb-item active">Crear Compra</li>
+        <li class="breadcrumb-item active">Registrar Compra</li>
     </ol>
 </div>
     <div class="container w-100 border border-3 border-primary rounded p-4 mt-3" >
         <form action="{{ route('purchase.store') }}" method="post">
             @csrf
-
             <div class="container mt-4">
                 <div class="row gy-4">
                     {{-- Product-Purchase --}}
@@ -59,15 +58,14 @@
 
                                 {{-- button --}}
                                 <div class="col-md-12 mb-2 text-end">
-                                    <button class="btn btn-primary" type="button">Agregar</button>
+                                    <button id="btn_add" class="btn btn-primary" type="button">Agregar</button>
                                 </div>
 
-                                {{--  Detail Table --}}
+                                {{-- Detail Table --}}
                                 <div class="col-12">
                                     <div class="table-responsive">
-                                        <table id="detail-table" class="table table-hover">
+                                        <table id="detail_table" class="table table-hover">
                                             <thead class="bg-primary">
-
                                                 <tr>
                                                     <th class="text-white">#</th>
                                                     <th class="text-white">Producto</th>
@@ -75,7 +73,8 @@
                                                     <th class="text-white">Precio compra</th>
                                                     <th class="text-white">Precio venta</th>
                                                     <th class="text-white">Subtotal</th>
-                                                    <th></th>
+                                                    <th>
+                                                    </th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -92,22 +91,29 @@
                                             <tfoot>
                                                 <tr>
                                                     <th></th>
-                                                    <th>Sumas</th>
-                                                    <th>0</th>
+                                                    <th colspan="4">Sumas</th>
+                                                    <th colspan="2"><span id="amount">0</span></th>
                                                 </tr>
                                                 <tr>
                                                     <th></th>
-                                                    <th>IVA %</th>
-                                                    <th>0</th>
+                                                    <th colspan="4">IVA %</th>
+                                                    <th colspan="2"><span id="iva">0</span></th>
                                                 </tr>
                                                 <tr>
                                                     <th></th>
-                                                    <th>Total</th>
-                                                    <th>0</th>
+                                                    <th colspan="4">Total</th>
+                                                    <th colspan="2"><span id="total">0</span></th>
                                                 </tr>
                                             </tfoot>
                                         </table>
                                     </div>
+                                </div>
+
+                                {{-- Cancel Button --}}
+                                <div class="col-md-12 mb-2 text-end">
+                                    <button id="cancelButton" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#cancelModal">
+                                        Cancelar compra
+                                    </button>
                                 </div>
 
                             </div>
@@ -162,11 +168,28 @@
 
                              {{-- Buttons --}}
                              <div class="col-md-12 mb-12 text-center">
-                                <button type="submit" class="btn btn-success">Guardar</button>
+                                <button id="saveButton" type="submit" class="btn btn-success">Guardar</button>
                              </div>
-
                           </div>
                       </div>
+                    </div>
+                </div>
+            </div>
+            <!-- cancel purchase Modal -->
+            <div class="modal fade" id="cancelModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Advertencia</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ¿Seguro que quieres cancelar la compra?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button id="btnCancelPurchase" type="button" class="btn btn-danger" data-bs-dismiss="modal">Confirmar</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -176,4 +199,165 @@
 
 @push('js')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#btn_add').click(function() {
+            addProduct();
+        });
+
+        $('#btnCancelPurchase').click(function() {
+            cancelPurchase();
+        });
+
+        disableButtons();
+
+        $('#tax').val(tax + '%')
+    });
+
+    function disableButtons(){
+        if( total == 0 ){
+            $('#saveButton').hide();
+            $('#cancelButton').hide();
+        }else{
+            $('#saveButton').show();
+            $('#cancelButton').show();
+        }
+    }
+
+    let cont = 0;
+    let subTotal = [];
+    let amount = 0;
+    let iva = 0;
+    let total = 0;
+
+    const tax = 16;
+
+    function cancelPurchase(){
+        $('#detail_table tbody').empty();
+
+        let row = '<tr>' +
+            '<th></th>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '</tr>';
+        $('#detail_table').append(row);
+
+        cont = 0;
+        subTotal = [];
+        amount = 0;
+        iva = 0;
+        total = 0;
+
+        $('#amount').html(amount);
+        $('#iva').html(iva);
+        $('#total').html(total);
+
+        cleanFields();
+        disableButtons();
+    }
+
+    function addProduct(){
+        let idProduct = $('#product_id').val();
+        let nameProduct = ($('#product_id option:selected').text()).split('  ')[1];
+        let quantity = $('#quantity').val();
+        let purchasePrice = $('#purchase_price').val();
+        let salePrice = $('#sale_price').val();
+
+     if (nameProduct != '' && nameProduct != undefined && quantity != '' && purchasePrice != '' && salePrice != ''){
+
+        if( parseInt(quantity) > 0 && ( quantity % 1 == 0 ) && parseFloat(purchasePrice) > 0 && parseFloat(salePrice) > 0 ){
+
+            if( parseFloat(salePrice) >= parseFloat(purchasePrice) ){
+
+                subTotal[cont] = round( quantity * purchasePrice );
+                amount += subTotal[cont];
+                iva = round( (amount / 100) * tax );
+                total = round( amount + iva );
+
+                let row = '<tr id="row'+ cont +'">' +
+                        '<th>' + (cont + 1) + '</th>' +
+                        '<td>' + nameProduct + '</td>' +
+                        '<td>' + quantity + '</td>' +
+                        '<td>$ ' + purchasePrice + '</td>' +
+                        '<td>$ ' + salePrice + '</td>' +
+                        '<td>$ ' + subTotal[cont]+ '</td>' +
+                        '<td><button class="btn btn-danger" type="button" onClick="deleteProduct('+ cont +')"><i class="fa-solid fa-trash"></i></button></td>' +
+                        '</tr>';
+                $('#detail_table').append(row);
+                cleanFields();
+                cont++;
+                disableButtons();
+
+                $('#amount').html(amount);
+                $('#iva').html(iva);
+                $('#total').html(total);
+            }else{
+                showModal('El precio de venta tiene que ser mayor que el de compra.')
+            }
+
+        } else{
+            showModal('Valores Incorrectos.')
+        }
+
+     } else{
+            showModal('Hacen falta campos por completar.');
+       }
+    }
+
+    function deleteProduct(index){
+        amount -= round(subTotal[index]);
+        iva = round( (amount / 100) * tax );
+        total = round( amount + iva );
+
+        $('#amount').html(amount);
+        $('#iva').html(iva);
+        $('#total').html(total);
+        $('#row'+ index).remove();
+    }
+
+    function cleanFields(){
+        let select = $('#product_id');
+        select.selectpicker('val', '');
+        $('#quantity').val('');
+        $('#purchase_price').val('');
+        $('#sale_price').val('');
+    }
+
+    function round(num, decimales = 2) {
+        var signo = (num >= 0 ? 1 : -1);
+        num = num * signo;
+        if (decimales === 0) //con 0 decimales
+            return signo * Math.round(num);
+        // round(x * 10 ^ decimales)
+        num = num.toString().split('e');
+        num = Math.round(+(num[0] + 'e' + (num[1] ? (+num[1] + decimales) : decimales)));
+        // x * 10 ^ (-decimales)
+        num = num.toString().split('e');
+        return signo * (num[0] + 'e' + (num[1] ? (+num[1] - decimales) : -decimales));
+    }
+
+    function showModal(message, icon = 'error') {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: icon,
+            title: message
+        })
+    }
+
+</script>
 @endpush
